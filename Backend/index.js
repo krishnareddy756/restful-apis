33d18e7748a6199a97a38de8d45ecccd55e54532
krishnareddy756 <saikrishnareddykalvapalli00@gmail.com>
2025-09-01 16:36:16 +0530

    Finished Create And New Route

--- Backend/index.js
+++ Backend/index.js
@@ -24,6 +24,14 @@ let posts=[
 app.get('/posts',(req,res)=>{
   res.render('index',{posts});
 })
+app.get('/posts/new',(req,res)=>{
+  res.render('new');
+})
+app.post('/posts',(req,res)=>{
+  let { username, content } = req.body;
+  posts.push({ username, content });
+  res.redirect('/posts');
+})
 app.listen(port,()=>{
   console.log(`Server is running on port ${port}`);
 })
